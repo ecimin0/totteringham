d52041bef6ab126c98ad26c83b1093600bf2ae78
Eric Cimino <design.eric.cimino@gmail.com>
2023-01-27 06:20:26 -0600

    profiling, updates

--- site/totteringham/__init__.py
+++ site/totteringham/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 # from sqlalchemy.sql import select
 from dotenv import load_dotenv
+from werkzeug.middleware.profiler import ProfilerMiddleware
 
 load_dotenv()
 
@@ -11,6 +12,14 @@ db = SQLAlchemy()
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
+
+    # app.wsgi_app = ProfilerMiddleware(
+    #     app.wsgi_app,
+    #     # restrictions=[30],
+    #     profile_dir='./profs',
+    #     filename_format="{method}-{path}-{time:.0f}-{elapsed:.0f}ms.prof",
+    # )
+
     app.config.from_mapping(
         SECRET_KEY='should_definitely_not_be_in_git',
         # DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
--- site/totteringham/main.py
+++ site/totteringham/main.py
@@ -10,9 +10,12 @@ from sqlalchemy.sql import select, or_
 import datetime
 import re
 import json
+import math
+from typing import Optional
 
 bp = Blueprint('main', __name__)
-
+# bp.wsgi_app = ProfilerMiddleware(bp)
+# print(bp.wsgi_app)
 
 @bp.route('/health', methods=['GET'])
 def health():
@@ -32,13 +35,100 @@ def index():
         afc_past_fix_complete = addFixtureWinner(afc_past_fix)
         spuds_past_fix_complete = addFixtureWinner(spuds_past_fix)
 
+        def currentPoints(fixtures):
+            points = 0
+            for f in fixtures:
+                if f.winner == "Arsenal" or f.winner == "Tottenham":
+                    points += 3
+                elif f.winner == "draw":
+                    points += 1
+                else:
+                    pass
+            return points
+
+        afc_points = currentPoints(afc_past_fix_complete)
+        spuds_points = currentPoints(spuds_past_fix_complete)
+
         afc_remaining_points = remainingPoints(afc_future_fix)
         spuds_remaining_points = remainingPoints(spuds_future_fix)
 
-        return render_template('index.html', afc_past=[a.toJson() for a in afc_past_fix_complete], afc_future=[a.toJson() for a in afc_future_fix], 
-            spuds_past=[a.toJson() for a in spuds_past_fix_complete], spuds_future=[a.toJson() for a in spuds_future_fix], 
+        # dates
+        afc_max_points = afc_points + afc_remaining_points
+        num_afc_matches_remaining = len(afc_future_fix)
+        spuds_max_points = spuds_points + spuds_remaining_points
+        num_spuds_matches_remaining = len(spuds_future_fix)
+        # print(f"afc pts: {afc_points}, afc max: {afc_max_points}, afc remain: {num_afc_matches_remaining} | tot pts: {spuds_points}, tot max: {spuds_max_points}, tot remain: {num_spuds_matches_remaining}")
+        # print(f"weeks: {(spuds_max_points - afc_points) / 3}, rounded: {math.ceil((spuds_max_points - afc_points) / 3)}")
+        # print(afc_future_fix[math.ceil(num_afc_matches_remaining - (spuds_max_points - afc_points) / 3)].event_date)
+
+
+        test_earliest()
+        # test_latest()
+        print(latest(50,36,18))
+        # print(earliest(50,36,18))
+
+        return render_template('index.html',
+            afc_past=[a.toJson() for a in afc_past_fix_complete],
+            afc_future=[a.toJson() for a in afc_future_fix],
+            spuds_past=[a.toJson() for a in spuds_past_fix_complete],
+            spuds_future=[a.toJson() for a in spuds_future_fix],
             nlds=playedNLD(afc_fix),
-            afc_points=afc_remaining_points, spuds_points=spuds_remaining_points)
+            afc_points=afc_points,
+            spuds_points=spuds_points,
+            afc_remaining_points=afc_remaining_points,
+            spuds_remaining_points=spuds_remaining_points)
+
+
+def earliest(main, opponent, remaining) -> Optional[int]:
+    for i in range(remaining):
+        print(f"Iteration: {i} Main: {main} Opponent: {opponent} Max Points Still Attainable: {(remaining - i) * 3} Perfect: {remaining * 3}")
+        if isTotts(main, opponent, remaining - i):
+            return i
+        main += 3
+        opponent += 0
+    return None
+
+def averagest(main, opponent, remaining) -> Optional[int]:
+    ...
+
+def latest(main, opponent, remaining) -> Optional[int]:
+    for i in range(remaining):
+        print(f"Iteration: {i} Main: {main} Opponent: {opponent} Max Points Still Attainable: {(remaining - i) * 3} Perfect: {remaining * 3}")
+        if isTottsLatest(main, opponent):
+            return i
+        main += 0
+        opponent += 3
+    return None
+
+def isTotts(main, opponent, remaining):
+    return main - opponent > remaining * 3
+
+def isTottsLatest(main, opponent):
+    return main > opponent
+
+def test_earliest():
+    assert(earliest(50, 36, 18) == 7)
+    print("")
+    # assert(earliest(50, 36, 1) == 0)
+    # print("")
+    # assert(earliest(0, 0, 3) == 2)
+    # print("")
+    # assert(earliest(0, 0, 18) == 10)
+
+def test_latest():
+    assert(latest(50, 36, 18) == 7)
+    print("")
+    # assert(earliest(50, 36, 1) == 0)
+    # print("")
+    # assert(earliest(0, 0, 3) == 2)
+    # print("")
+    # assert(earliest(0, 0, 18) == 10)
+
+
+# def possibleDates(points, remain_points, future_matches):
+#     highest_possible_points = points + remain_points
+#     num_matches_remaining = len(future_matches)
+
 
 def splitFixturesByTeam(fixtures):
     afc = []
